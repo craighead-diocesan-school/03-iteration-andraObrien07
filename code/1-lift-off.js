b26308109f7afdab216ac520b462431a120e2afb
andraObrien07 <comanan09@craighead.school.nz>
2023-06-07 14:47:04 +1200

    make complete

--- code/1-lift-off.js
+++ code/1-lift-off.js
@@ -3,17 +3,18 @@
 // ####################################
 
 function liftOff() {
-  let count = 10
- 
-  while (count > 0) {
-    alert(count + '...')
-    count = count - 1
+  // let count = 10
+  let choice = prompt('Pick your number of choice to count down from.')
+
+  while (choice > 0) {
+    alert(choice + '...')
+    choice = choice - 1
   }
-   
+
   alert('Lift Off!')
 }
 
 function make1() {
   // copy and paste your code here when you get to the Make step
-  
+
 }
