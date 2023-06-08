834e42284ced6b146eb11166b518e35b2800d195
andraObrien07 <comanan09@craighead.school.nz>
2023-06-08 18:35:02 +1200

    got it to square root 5

--- code/3-square-series.js
+++ code/3-square-series.js
@@ -3,14 +3,35 @@
 // ####################################
 
 function squareSeries() {
-  // write your code here
+  //ask for the number that they want a series for
 
-  // ask for the number for the series
-  let rat = prompt('What number do you want the series for?')
-  let numbers = prom
+  //tell them the series 
+  // let question = prompt('What number do you want the series for? ')
 
-  // say the number and below all the number picked for the series
-  while () {
+  // upto = question
+  // let Thenumber = 10
+  // Thenumber = Number(Thenumber)
+  // question = Number(question)
+  // upto = Number(upto)
+  // while (upto < Thenumber) {
 
-  }
+  // Thenumber = Thenumber * Thenumber
+  // question = question * question
+  //   Thenumber = upto * upto
+  // }
+  // alert('The number is ' + Thenumber)
+
+
+  let Thenumber = prompt('what number do you want the series for?')
+
+
+  Thenumber = Number(Thenumber)
+
+  // let count = Thenumber
+  // count < Thenumber
+
+  let square = Thenumber * Thenumber
+
+  alert('The series for that number is ' + square)
+
 }
