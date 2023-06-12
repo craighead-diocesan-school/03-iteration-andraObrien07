3a635369cbbbc9ad8af2e42e63efdab59f50bec5
andraObrien07 <comanan09@craighead.school.nz>
2023-06-12 12:24:26 +1200

    anoyed

--- code/5-ext-fibonacci.js
+++ code/5-ext-fibonacci.js
@@ -4,4 +4,21 @@
 
 function fibonacci() {
   // write your code here
+
+  //8! = (0+1) = 1, 1+2 = 3, 2+3 =5, 3+5= 8
+
+  let fibs = prompt('what number do you the fibonacci sequence to?')
+  fibs = Number(fibs)
+  let count = 1
+  let seq = ''
+  let total = 0
+
+  while (count < fibs) {
+
+    seq = (total + count)
+    seq = seq + (total + count)
+
+  }
+
+  alert('The fibonacci sequence for ' + fibs + 'is ...' + seq)
 }
