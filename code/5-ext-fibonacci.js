7dfea291bee194291c95b18dc336e8ad25a72d13
andraObrien07 <comanan09@craighead.school.nz>
2023-07-01 17:54:41 +1200

    i give up

--- code/5-ext-fibonacci.js
+++ code/5-ext-fibonacci.js
@@ -5,70 +5,25 @@
 function fibonacci() {
   // write your code here
 
-  //8! = (0+1) = 1, 1+2 = 3, 2+3 =5, 3+5= 8
-
-  // let fibs = prompt('what number do you the fibonacci sequence to?')
-  // fibs = Number(fibs)
-  // let count = 1
-  // let seq = ''
-  // let total = 0
-
-  // while (count < fibs) {
-
-  //   seq = (total + count)
-  //   seq = seq + (total + count)
-
-  // }
-
-  // alert('The fibonacci sequence for ' + fibs + 'is ...' + seq)
-  // let number = prompt('What number do u want the fibonacci sequence for?')
-  // let red = next
-  // let orange = previous
-  // let green = old
-  // count = 1
-  // // let count = 0
-  // while (red <= number) {
-  //   green = orange
-  //   orange = red
-  //   red = green + orange
-
-  // }
-  // alert('sequence is 0, 1, ' + red)
-
 
   let number = prompt('What number do u want the fibonacci sequence for?')
-  // let red = count + 1
-  // let violet = red + 1
-  // let green = red + violet
+  number = Number(number)
 
+  let count = 0
+  count = Number(count)
 
+  let red = 0
+  let violet = 1
+  let green = ''
 
-  // let red = count
-  // let violet = red + red
-  // let green = violet + red
-
-  count = 1
-
-  let red = 1
-  let violet = 2
-  let green = 3
-
-
-  while (green <= number) {
-    //   violet = red + red
-    //   green = violet + red
-    //   blue = green + violet
-
-    // let green = count + red
-    // let sum = green + violet
-
-    red = count + 1
-    violet = red + count
-    violet = red + 1
+  let sequence = 'the fibonacci sequence: '
+  while (count < red) {
     green = red + violet
-
-
+    red = violet
+    violet = green
+    sequence = sequence + '' + green
+    count = green
   }
 
-  alert('The fibonacci sequence up to you number is ' + green)
+  alert(sequence)
 }
